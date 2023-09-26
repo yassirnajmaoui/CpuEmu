f5c50f3507d4acbb967e411238983a81ac3a0912
Yassir Najmaoui <yssrnjm@gmail.com>
2023-09-26 18:47:52 -0400

    Add RegistersWriter

--- include/Units/Registers.hpp
+++ include/Units/Registers.hpp
@@ -2,22 +2,17 @@
 
 #include "Types/Node.hpp"
 
-// TODO: Create a Register Write back class that allows to write in the registry
-// - Remove the Write operations in class Registers (only allow reading)
-// - The Write back class should do only reading and have a shared_ptr of
-//   the Registers object
-
 class Registers : public Node
 {
+
+friend class RegistersWriter;
+
 public:
 	static constexpr size_t NumberOfRegisters = 16;
 
 	// Inputs
-	static constexpr size_t RegWriteIndex = 0;
-	static constexpr size_t ReadRegister1Index = 1;
-	static constexpr size_t ReadRegister2Index = 2;
-	static constexpr size_t WriteRegisterIndex = 3;
-	static constexpr size_t WriteDataIndex = 4;
+	static constexpr size_t ReadRegister1Index = 0;
+	static constexpr size_t ReadRegister2Index = 1;
 
 	// Outputs
 	static constexpr size_t ReadData1Index = 0;
--- src/Units/Registers.cpp
+++ src/Units/Registers.cpp
@@ -3,18 +3,15 @@
 #include "Utils.hpp"
 
 Registers::Registers() :
-	Node(5, 2)
+	Node(2, 2)
 {
 }
 
 void Registers::ProcessInternal()
 {
 	// Setup
-	WireData lRegWrite = GetWireData(RegWriteIndex);
 	size_t lReadRegister1 = static_cast<size_t>(GetWireData(ReadRegister1Index));
 	size_t lReadRegister2 = static_cast<size_t>(GetWireData(ReadRegister2Index));
-	size_t lWriteRegister = static_cast<size_t>(GetWireData(WriteRegisterIndex));
-	WireData lWriteData = GetWireData(WriteDataIndex);
 
 	ASSERT_coherence(lReadRegister1 < NumberOfRegisters, "Register address 1 requested too high");
 	ASSERT_coherence(lReadRegister2 < NumberOfRegisters, "Register address 2 requested too high");
@@ -25,11 +22,4 @@ void Registers::ProcessInternal()
 
 	SetWireData(ReadData1Index, lContentsOfReg1);
 	SetWireData(ReadData2Index, lContentsOfReg2);
-
-	// Wire in register if RegWire is true
-	if(lRegWrite)
-	{
-		ASSERT_coherence(lWriteRegister < NumberOfRegisters, "Write Register address requested too high");
-		mRegisters[lWriteRegister] = lWriteData;
-	}
 }
